98300cbf41e02bf7ddcc2335310bc1b6a095dd0e
wooseok0727 <crash2@naver.com>
2021-11-23 15:32:34 +0900

    meett

---
+++ server/src/test/java/com/team/meett/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.meett;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
