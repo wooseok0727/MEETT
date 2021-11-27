8d942f2491668defb900091853193e249bc3c519
j-hm <thinkalonej@gmail.com>
2021-11-27 18:02:31 +0900

    UserScheduleEntity

---
+++ server/src/main/java/com/team/meett/model/UserSchedule.java
@@ -0,0 +1,47 @@
+package com.team.meett.model;
+
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+
+@Entity
+@Table(name = "tbl_schedule", schema = "MeettDB")
+public class UserSchedule {
+    @Id
+    @Column(name = "seq", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long seq;
+
+    @Column(length = 20, nullable = false)
+    private String username;
+
+    @Column(length = 20, nullable = false)
+    private String title;
+
+    @Column(length = 255, nullable = false)
+    private String detail;
+
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    @Column(nullable = false)
+    private Date start;
+
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    @Column(nullable = false)
+    private Date end;
+
+    @ColumnDefault("0")
+    private int role;
+
+}
