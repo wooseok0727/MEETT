687cc8bb5a3cd5d1671b0d6ca654b37873c0cccc
wooseok0727 <crash2@naver.com>
2021-11-27 23:39:14 +0900

    meett

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router";
+import "./App.css";
+import * as PAGE from "./pages";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<PAGE.HomePage />} />
+        <Route path="/mypage" element={<PAGE.MyPage />} />
+        <Route path="/login" element={<PAGE.LoginPage />} />
+        <Route path="/register" element={<PAGE.RegisterPage />} />
+        <Route path="/schedule" element={<PAGE.MySchedulePage />} />
+        <Route path="/team" element={<PAGE.TeamSchedulePage />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HomeNavbar from "../components/HomeNavbar";
+
+const LoginPage = () => {
+  return (
+    <>
+      <HomeNavbar />
+      <div>로그인</div>
+    </>
+  );
+};
+
+export default LoginPage;
