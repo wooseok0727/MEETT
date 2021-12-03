4c6bbf80e942a173bee4b339fe95f53551a71b90
rudgjs8080 <kkh_8080@naver.com>
2021-12-03 17:33:01 +0900

    1203 MEETT server db fk test~

--- server/src/test/java/com/team/meett/ServerApplicationTests.java
+++ server/src/test/java/com/team/meett/ServerApplicationTests.java
@@ -1,7 +1,13 @@
 package com.team.meett;
 
+import com.team.meett.model.User;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest
 class ServerApplicationTests {
@@ -9,5 +15,6 @@ class ServerApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
 
 }
