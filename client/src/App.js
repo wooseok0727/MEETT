595b648351d2ffa9b1a57e144a5d54396396478b
wooseok0727 <crash2@naver.com>
2021-12-13 14:56:47 +0900

    meett

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
+import AuthRoute from "./components/auth/AuthRoute";
 import * as PAGE from "./pages";
 
 const App = () => {
@@ -9,7 +10,14 @@ const App = () => {
         <Route path="/" element={<PAGE.HomePage />} />
         <Route path="/login" element={<PAGE.LoginPage />} />
         <Route path="/register" element={<PAGE.RegisterPage />} />
-        <Route path="/schedule/*" element={<PAGE.SchedulePage />} />
+        <Route
+          path="/schedule/*"
+          element={
+            <AuthRoute>
+              <PAGE.SchedulePage />
+            </AuthRoute>
+          }
+        />
       </Routes>
     </>
   );
