77c4058d38691343778fb2a57ae3bd8d08e7c55e
rudgjs8080 <kkh_8080@naver.com>
2021-12-15 15:41:51 +0900

    1215 key SearchService

--- server/src/main/java/com/team/meett/repository/TeamRepository.java
+++ server/src/main/java/com/team/meett/repository/TeamRepository.java
@@ -2,17 +2,10 @@ package com.team.meett.repository;
 
 import com.team.meett.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TeamRepository extends JpaRepository<Team, String> {
 
-
     public List<Team> findByTitle(String title);
-
-
-
-
 }
